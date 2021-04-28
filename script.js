0b35b440232178c8e7a0cba86d15df67ae957843
unknown776 <79632873+unknown776@users.noreply.github.com>
2021-04-28 19:55:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let a;
 let time;
-setInterval(() => {
+time = setInterval(() => {
     a = new Date();
     time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
     document.getElementById('time').innerHTML = time
 }, 1000);
+
+function btnFunc(){
+    clearInterval(time);
+    let str = "Bhai/behen tera bahut bura time chal rha hai. Waise to poori duniya ka bura time chal rha hai par tera kuchh alag hi mamla hai. Apna khayaal rakhna!"
+    document.getElementById('txt').innerHTML = str;
+    document.getElementById('btn').remove()
+}
